2616801b3517ca9928b544c277aa6a32e1f17972
Cecocho <acecocho@gmail.com>
2023-11-21 23:38:00 +0100

    learnings

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import styles from "./App.module.css";
+import { Button } from "./components/Button/Button";
 import { Panel } from "./components/Panel/Panel";
 
 function App() {
-    return (
-        <main className={styles.main}>
-            <Panel />
-        </main>
-    );
+  const [isPanelShown, setIsPanelShown] = useState(true);
+
+  return (
+    <main className={styles.main}>
+      <Button
+        onClick={() => {
+          setIsPanelShown((prevIsPanelShown) => !prevIsPanelShown);
+        }}
+      >
+        {isPanelShown ? "Schowaj panel" : "Pokaź panel"}
+      </Button>
+      {isPanelShown && <Panel />}
+    </main>
+  );
 }
 
 export default App;
--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../Button/Button";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            word: "kot",
-            translation: "cat",
-            category: "noun",
-        },
-        {
-            id: 2,
-            word: "pies",
-            translation: "dog",
-            category: "noun",
-        },
-        {
-            id: 3,
-            word: "skakać",
-            translation: "jump",
-            category: "verb",
-        },
-    ]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    return (
-        <>
-            <Button>Załaduje dane</Button>
-            <section className={styles.section}>
-                <List data={data}></List>
-            </section>
-        </>
-    );
+  useEffect(() => {
+    handleLoadClick();
+  }, []);
+
+  useEffect(() => {
+    console.log("dupa!");
+  }, []);
+
+  function handleLoadClick() {
+    setIsLoading(true);
+    fetch("http://localhost:3000/words")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setIsLoading(false);
+      });
+  }
+  return isLoading ? (
+    <p>Ładowanie gówna ziomeczQu</p>
+  ) : (
+    <>
+      <section className={styles.section}>
+        <List data={data}></List>
+      </section>
+    </>
+  );
 }
